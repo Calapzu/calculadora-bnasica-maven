abd3640ff8f5bc590b70373031463f1ed1cf87d5
Calapzu <yamsoncalapzu@gmail.com>
2021-09-27 01:25:20 -0500

    Se terminan de hacer los metodos de la calculadora basica y se construyen los test de suma, resta y multiplicacion

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,2 +1,96 @@
-package co.com.sofka.app.calculator;public class BasicCalculatorTest {
+package co.com.sofka.app.calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BasicCalculatorTest {
+    private final BasicCalculator basicCalculator = new BasicCalculator();
+
+    @Test
+    @DisplayName("Testing sum: 1+1=2")
+    public void sum() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 2L;
+
+        // Act
+        Long result = basicCalculator.sum(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("Testing subtract: 1-1=0")
+    public void subtract() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 0L;
+
+        // Act
+        Long result = basicCalculator.subtract(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several subtracts")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "1,    1,   0",
+            "12,    7,   5",
+            "49,  51, -2",
+            "100,  1, 99"
+    })
+    public void severalSubtracts(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.subtract(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
+
+    @Test
+    @DisplayName("Testing multiply: 1*1=1")
+    public void multiply() {
+        // Arrange
+        Long number1 = 1L;
+        Long number2 = 1L;
+        Long expectedValue = 1L;
+
+        // Act
+        Long result = basicCalculator.multiply(number1, number2);
+
+        // Assert
+        assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several multiplications")
+    @ParameterizedTest(name = "{0} * {1} = {1}")
+    @CsvSource({
+            "1,    1,   1",
+            "12,    7,   84",
+            "49,  51, 2499",
+            "100,  1, 100"
+    })
+    public void severalMultiplications(Long first, Long second, Long expectedResult) {
+        assertEquals(expectedResult, basicCalculator.multiply(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
 }
